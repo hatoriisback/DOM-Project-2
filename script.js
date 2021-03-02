3debc67e1aec879c8b6845181a320a66bce0b2ff
hendhendy <hendy.maxv@gmail.com>
2021-03-02 16:20:56 +0700

    DONE

---
+++ script.js
@@ -0,0 +1,65 @@
+// // Modal Window // //
+
+// 'use strict';
+
+// const modal = document.querySelector('.modal');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelector('.close-modal');
+// const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// const openModal = function () {
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// };
+
+// const closeModal = function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// };
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   // Open Modal
+//   btnsOpenModal[i].addEventListener('click', openModal);
+// // Close Modal
+// btnCloseModal.addEventListener('click', closeModal);
+// overlay.addEventListener('click', closeModal);
+
+// document.addEventListener('keydown', function (e) {
+//   // console.log(e.key);
+
+//   // Cara 1
+//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+//     closeModal();
+//   }
+
+  // // Cara 2 )
+  //   if (e.key === 'Escape') {
+  //     if (!modal.classList.contains('hidden')) {
+  //       closeModal();
+  //     }
+  //   }
+// });
+/*  */
+
+
+// ====================================================================
+/*  */
+
+
+// // Old Ways // //
+
+//   btnsOpenModal[i].addEventListener('click', function () {
+//     console.log('Button clicked');
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+//   });
+
+//   btnCloseModal.addEventListener('click', function () {
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+//   });
+
+//   overlay.addEventListener('click', function () {
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+//   });
